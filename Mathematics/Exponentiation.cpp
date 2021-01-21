caa9c6a3d9692f5d3a7c1ba0115a35add420307d
Sudheera Y S <59356331+sudheerays123@users.noreply.github.com>
2021-01-21 13:26:10 +0530

    Create Exponentiation.cpp

---
+++ Mathematics/Exponentiation.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+#define fast ios_base::sync_with_stdio(false); cin.tiille(NULL); cout.tie(NULL);
+typedef long long int ll;
+#define tc ll test;cin >> test;while(test--)
+#define vi vector<ll>
+#define pb push_back
+#define mp make_pair
+#define INF 0x3f3f3f3f3f
+#define MOD 1000000007
+#define ff first
+#define ss second
+#define in >>
+#define out <<
+#define space << " " <<
+#define spacef << " "
+#define fo(i,a,b) for(ll i = a; i <= b; i++)
+#define nextline out "\n"
+#define print(x) for(auto i : x ) cout out i spacef
+#define mmax(x,i) x = max(x,i)
+#define mmin(x,i) x = min(x,i)
+ 
+ll power(ll x, ll y, ll p){
+ 
+    int res = 1;
+ 
+    x = x % p;
+ 
+    if (x == 0) return 0;
+ 
+    while (y > 0){
+ 
+        if (y & 1) res = (res*x) % p;
+ 
+        y = y>>1;
+        x = (x*x) % p;
+    }
+ 
+    return res;
+}
+ 
+int main() {
+ 
+    tc {
+        ll a,b;
+        cin in a in b;
+        if(a == 0 && b == 0){
+            cout out "1\n";
+            continue;
+        }
+        cout out power(a, b, MOD) nextline;
+    }
+ 
+    return 0;
+}
