238734d98b7427b59eb8be39d66ac2456569ca90
Sudheera Y S <59356331+sudheerays123@users.noreply.github.com>
2021-01-22 19:30:39 +0530

    Update Building Teams.cpp

--- Graph Algorithms/Building Teams.cpp
+++ Graph Algorithms/Building Teams.cpp
@@ -19,55 +19,55 @@ typedef long long int ll;
 #define print(x) for(auto i : x ) cout out i;
 #define mmax(x,i) x = max(x,i)
 #define mmin(x,i) x = min(x,i)
-
+ 
 vi adj[100005];
 vector<bool> visited(100005);
 vi ans(100005,INF);
 bool pos = true;
-
+ 
 void dfs(ll s){
     if(visited[s] == true) return;
     visited[s] = true;
-
+ 
     for(auto u : adj[s]) {
-        if ((visited[u] == false && ans[s] == 2 && ans[u] == 2) || (visited[u] == false && ans[s] == 1 && ans[u] == 1)) {
+        if ((visited[u] == false && ans[s] == ans[u]) || (visited[u] == false && ans[s] == ans[u])) {
             pos = false;
             return;
         }
     }
-
+ 
     for(auto u : adj[s]){
         if(visited[u] == true || ans[u] != INF) continue;
         if(ans[s] == 1) ans[u] = 2;
         else ans[u] = 1;
     }
-
+ 
     for(auto u : adj[s]){
         dfs(u);
     }
 }
-
+ 
 int main() {
-
+ 
     ll n,m;
     cin in n in m;
-
+ 
     fo(i,0,m-1){
         ll a,b;
         cin in a in b;
         adj[a-1].pb(b-1);
         adj[b-1].pb(a-1);
     }
-
+ 
     fo(i,0,n-1){
         if(visited[i] == false){
             ans[i] = 1;
             dfs(i);
         }
     }
-
+ 
     if(pos == true ) fo(i,0,n-1) cout out ans[i] spacef;
     else cout out "IMPOSSIBLE\n";
-
+ 
     return 0;
 }
