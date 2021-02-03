292c6c96b50347fa4daca2b141cde53ae40a0166
Sudheera Y S <59356331+sudheerays123@users.noreply.github.com>
2021-02-03 14:44:18 +0530

    Create Factory Machines.cpp

---
+++ Sorting and Searching/Factory Machines.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+using namespace std;
+#define fast ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+typedef long long int ll;
+#define tc ll test;cin >> test;while(test--)
+#define vi vector<ll>
+#define pb push_back
+#define mp make_pair
+#define INF 0x3f3f3f3f3f
+#define MOD 1000000007
+#define ff first
+#define ss second
+#define in >>
+#define out <<
+#define space << " " <<
+#define spacef << " "
+#define fo(i,a,b) for(ll i = a; i <= b; i++)
+#define nextline out "\n"
+#define print(x) for(auto i : x ) cout out i spacef;
+#define mmax(x,i) x = max(x,i)
+#define mmin(x,i) x = min(x,i)
+
+
+int main() {
+
+    ll n,t;
+    ll sum = 0;
+
+    cin in n in t;
+
+    vi k(n+5);
+
+    fo(i,0,n-1) {
+        cin in k[i];
+        sum += k[i];
+    }
+
+    ll low = 0;
+    ll high = 1e18;
+    ll ans;
+
+    while(low <= high){
+
+        ll mid = (low+high)/2;
+
+        ll thist = 0;
+        fo(i,0,n-1) {
+            thist += mid/k[i];
+            if(thist >= t) break;
+        }
+
+        if(thist >= t){
+            ans = mid;
+            high = mid-1;
+        }
+        else{
+            low = mid+1;
+        }
+    }
+
+    cout out ans;
+
+    return 0;
+}
